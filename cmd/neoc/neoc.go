13c710ec57af39f4af5bb593e1050ac8e1e6dc59
Philip K <philip@warpmail.net>
2019-07-02 12:55:00 +0200

    Reworked client for new API

--- cmd/neoc/neoc.go
+++ cmd/neoc/neoc.go
@@ -10,120 +10,119 @@ import (
 	"github.com/phikal/neoc"
 )
 
-func main() {
-	user := flag.String("user", os.Getenv("NCU"),
-		"Set username (if not set, it will default to $NCU)")
-	pass := flag.String("pass", os.Getenv("NCP"),
-		"Set password (if not set, it will default to $NCP)")
-	verb := flag.Bool("v", false, "output verbosely")
+var verbose bool
 
-	flag.Parse()
-	if flag.NArg() == 0 {
-		fmt.Printf("usage: %s [upload|delete|sync|push|list] [file o. dir]\n", os.Args[0])
-		fmt.Printf("       Use -h for information on username and passwords\n")
-		os.Exit(1)
-	}
-
-	if *user == "" {
-		fmt.Printf("Username not defined\n")
-		os.Exit(1)
-	}
-
-	if *pass == "" {
-		fmt.Printf("Password not defined\n")
-		os.Exit(1)
-	}
-
-	var (
-		err  error
-		size int
-		list []neoc.Item
-	)
-
-	switch strings.ToLower(flag.Arg(0))[0] {
+func do(c *neoc.Client, mode byte) error {
+	switch mode {
 	case 'u': // upload
 		if flag.NArg() < 2 {
 			fmt.Printf("UPLOAD request requires arguments\n")
-			os.Exit(3)
+			os.Exit(1)
 		}
 
-		if *verb {
+		if verbose {
 			fmt.Println("Uploading...")
 		}
-		err = neoc.Upload(flag.Args()[1:], *user, *pass)
+		return c.Upload(flag.Args()[1:])
 	case 'd': // delete
 		if flag.NArg() < 2 {
 			fmt.Printf("DELETE request requires arguments\n")
-			os.Exit(3)
+			os.Exit(1)
 		}
 
-		if *verb {
+		if verbose {
 			fmt.Println("Deleting...")
 		}
-		err = neoc.Delete(flag.Args()[1:], *user, *pass)
+		return c.Delete(flag.Args()[1:])
 	case 's': // sync
-		ch := make(chan string)
-		size, err = neoc.Sync(flag.Arg(1), ch, *user, *pass)
+		ch := make(chan *neoc.Item)
+		size, err := c.Sync(flag.Arg(1), ch)
+		if err != nil {
+			return err
+		}
 
-		if *verb {
+		if verbose {
 			fmt.Println("Downloading...")
 		}
 
-		for i := 1; i <= size; i++ {
-			msg, ok := <-ch
-
-			if !ok {
-				break
-			}
-
-			if msg == "" {
+		i := 1
+		for item := range ch {
+			if item != nil {
 				continue
 			}
 
-			if *verb {
-				fmt.Printf("[%d/%d] %s...\n", i, size, msg)
+			if verbose {
+				fmt.Printf("[%d/%d] %s...\n", i, size, item.Path)
+				i++
 			}
 		}
 	case 'p': // push
-		ch := make(chan string, 1<<5)
-		err = neoc.Push(flag.Arg(1), ch, *user, *pass)
+		ch := make(chan *neoc.Item)
+		err := c.Push(flag.Arg(1), ch)
+		if err != nil {
+			return err
+		}
 
-		if *verb {
+		if verbose {
 			fmt.Println("Pushing...")
 		}
 
-		var flist []string
-		for {
-			msg, ok := <-ch
-			if !ok {
-				break
-			}
-
-			flist = append(flist, msg)
-			if *verb {
+		var items []*neoc.Item
+		for msg := range ch {
+			items = append(items, msg)
+			if verbose {
 				fmt.Println(msg)
 			}
 		}
 
-		neoc.Upload(flist, *user, *pass)
+		var files []string
+		for _, i := range items {
+			files = append(files, i.Path)
+		}
+		return c.Upload(files)
 	case 'l': // list
-		if *verb {
+		if verbose {
 			fmt.Println("Loading...")
 		}
-		list, err = neoc.List(*user, *pass)
+		list, err := c.List()
+		if err != nil {
+			return err
+		}
 
-		for _, itm := range list {
-			fmt.Printf("%10d %s %s\n", itm.Size, itm.Updated.Format(time.Stamp), itm.Path)
+		for _, item := range list {
+			fmt.Printf("%10d %s %s\n", item.Size, item.Updated.Format(time.Stamp), item.Path)
 		}
 	}
+	return nil
+}
 
+func main() {
+	var user, pass string
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `usage: %s [upload|delete|sync|push|list] [file or directory]*`, os.Args[0])
+		if user != "" {
+			fmt.Fprintln(os.Stderr, "warning: username is empty")
+		}
+		if pass != "" {
+			fmt.Fprintln(os.Stderr, "warning: password is empty")
+		}
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&user, "user", os.Getenv("NEOCITIES_USER"), "Set username (if not set, it will default to $NEOCITIES_USER)")
+	flag.StringVar(&pass, "pass", os.Getenv("NEOCITIES_PASS"), "Set password (if not set, it will default to $NEOCITIES_PASS)")
+	flag.BoolVar(&verbose, "v", false, "turn on verbose output")
+
+	flag.Parse()
+	if flag.NArg() == 0 || user == "" || pass == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err := do(neoc.NewClient(user, pass), strings.ToLower(flag.Arg(0))[0])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(2)
+		os.Exit(1)
 	}
-
-	if *verb {
-		fmt.Println("Done")
-	}
-
 }
